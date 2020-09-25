90b878f0db5f65b379868040ce50ecbcab440eea
klcysn <klcysn@users.noreply.github.com>
2020-09-25 09:19:08 +0200

    start js project

--- javascript/projects/002-calculator/myScript.js
+++ javascript/projects/002-calculator/myScript.js
@@ -1,6 +1,9 @@
-document.querySelectorAll(".number").addEventListener("click", bottomScreen)
-
-function bottomScreen() {
-    document.querySelector(".bottom_screen").innerText = document.querySelectorAll(".number").value
-}
+//Giving variables
+const num = document.querySelectorAll(".number")
+const opt = document.querySelectorAll(".operator")
+const equal = document.querySelector(".equal")
+const del = document.querySelector(".del")
+const clear = document.querySelector(".ac")
+const topScreen = document.querySelector(".top_screen")
+const bottomScreen = document.querySelector(".bottom_screen")
 
