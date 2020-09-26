e75967869a266d4cc0f7702fec76d47441367a7b
klcysn <klcysn@users.noreply.github.com>
2020-09-26 18:18:32 +0200

    use map method

--- sprint-evaluation/sprint/sprint2/myScript.js
+++ sprint-evaluation/sprint/sprint2/myScript.js
@@ -3,20 +3,18 @@ let times = new Date();
 let hour = times.getHours();
 let minute = times.getMinutes();
 let second = times.getSeconds();
-let timeList = [];
-timeList.push(second, minute, hour);
-let timeList_2 = ["AM"];
+let timeList = [hour, minute, second];
 
-timeList.forEach((p) => {
-    if(p < 10){
-        p = "0" + p.toString()
-    }
-    timeList_2.unshift(p)
-});
+let timeList_2 = timeList.map((p) =>{
+    if(p < 10){return "0" + p
+}else {return p}
+})
+
+timeList_2.push("AM");
 
 if(timeList_2[0] > 12){
     timeList_2[0] = timeList_2[0] - 12;
-    timeList_2[timeList_2.length - 1] = "PM";
+    timeList_2[3] = "PM";
     if(timeList_2[0] < 10){
         timeList_2[0] = "0" + timeList_2[0]
     }
