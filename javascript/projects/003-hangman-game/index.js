b7b7111f4e6c964da34279ce7416d7e46ab0413a
klcysn <klcysn@users.noreply.github.com>
2020-09-28 11:41:37 +0200

    create word list. create random word chooser, element maker. define only letter input.

--- javascript/projects/003-hangman-game/index.js
+++ javascript/projects/003-hangman-game/index.js
@@ -2,18 +2,20 @@ const wordList = ["absurd", "awkward", "clarusway", "funny", "galaxy", "joking",
 const i = Math.floor(Math.random() * 20) + 1;
 const word = wordList[i].split("");
 console.log("word", word)
-word.forEach(() => {
-    
+word.forEach((letter) => {
+    let division = document.createElement("div")
+    division.classList.add("letter")
+    division.innerHTML = letter
+    document.querySelector(".word").appendChild(division)
 })
 document.querySelector("body").addEventListener("keyup", (letter) => {
-console.log("letter", letter)
     if(letter.keyCode >= 65 && letter.keyCode <= 90){
-        if(word.includes(letter.key)){
-            alert("okkk")
-        }
+        word.forEach((x) => {
+            if(x == letter.key){
+                document.querySelectorAll(".letter").style.color = "white";
+            }
+        });
     }else{return}
 });
 
-function chooser(){
-      
-};
+
