86f2152688a5ecdf51a0fe7d7f40271e1cb414ce
klcysn <klcysn@users.noreply.github.com>
2020-09-28 18:20:42 +0200

    add wrong word division, add wordDiv to compare all same letters

--- javascript/projects/003-hangman-game/index.js
+++ javascript/projects/003-hangman-game/index.js
@@ -1,21 +1,35 @@
 const wordList = ["absurd", "awkward", "clarusway", "funny", "galaxy", "joking", "fullstack", "strength", "puzzling", "kilobyte", "keyhole", "cycle", "loop", "slack", "function", "fixable", "buzzard", "avenue", "lengths", "monitor", "information"];
 const i = Math.floor(Math.random() * 20) + 1;
 const word = wordList[i].split("");
-console.log("word", word)
+console.log("word", word);
+let wordDiv =[];
 word.forEach((letter) => {
     let division = document.createElement("div")
     division.classList.add("letter")
     division.innerHTML = letter
     document.querySelector(".word").appendChild(division)
+    wordDiv.push(division)
 })
+console.log(wordDiv)
+console.log(document.querySelector(".word"))
 document.querySelector("body").addEventListener("keyup", (letter) => {
     if(letter.keyCode >= 65 && letter.keyCode <= 90){
-        word.forEach((x) => {
-            if(x == letter.key){
-                document.querySelectorAll(".letter").style.color = "white";
+        if(!word.includes(letter.key)){
+            document.querySelector(".wrong_words").textContent += letter.key;
+            return
+        }else {
+        
+            wordDiv.forEach((x) => {
+            if(x.innerText == letter.key){
+                x.style.color = "white";
             }
         });
-    }else{return}
+        
+        }
+         
+    
+    }else return
 });
+
 
 
