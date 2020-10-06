3b7ae1f2c84b8fec77afb9f1fe5b150cfa3fc629
klcysn <klcysn@users.noreply.github.com>
2020-10-06 13:25:22 +0200

    Start news project for android

---
+++ react-native/projects/newsProject/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, View, SafeAreaView, Button, Image} from "react-native"; 
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>News</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+export default App;
