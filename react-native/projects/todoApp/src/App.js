0787d1c3cf66b7ea378aa3650d948fce519c1afa
klcysn <klcysn@users.noreply.github.com>
2020-10-13 15:13:03 +0200

    create Task function

--- react-native/projects/todoApp/src/App.js
+++ react-native/projects/todoApp/src/App.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
   TextInput,
   TouchableOpacity,
   Dimensions,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
 } from 'react-native';
+ 
 
 
 
 const App = () => {
+  const todoList = [3, 2]
+  const [task, setTask] = useState("")
+  const Task = ()=>{
+    return(
+     todoList.map((item)=>{
+       return(
+         <View style ={{flex : 1, flexDirection : "row", alignItems : "center"}}>
+          <TouchableOpacity style={{flex : 1}}>
+            <Text style ={{color : "white", fontSize : 20, backgroundColor :"#546E7A", padding : 5, textAlign : "center"}}>X</Text>
+          </TouchableOpacity>
+          <Text style={{color : "white", fontSize : 20, backgroundColor :"#546E7A", margin : 2, padding : 5, flex : 9}}>{item}</Text>
+         </View>
+        
+       )
+     })
+    )
+  }
+
   return (
     <SafeAreaView style = {styles.container}>
       <KeyboardAvoidingView style = {{flex : 1}}>
         <View style ={styles.head}>
           <Text style ={styles.text}>TODO</Text>
-          <Text style ={styles.text}>0</Text>
+          <Text style ={styles.text}>{todoList.length}</Text>
         </View>
         <View  style = {styles.screen}>
           <ScrollView>
-            <Text>Yap</Text>
+            <Task />
           </ScrollView>
         </View>
         
         <View style = {styles.buttonContainer}>
-          <TextInput placeholder = "Write a task!" style={styles.input}/>
-          <TouchableOpacity style = {styles.button}>
+          <TextInput placeholder = "Write a task!" style={styles.input} onChangeText ={(text) =>{setTask(text)}}/>
+          <TouchableOpacity style = {styles.button} onPress ={()=>{
+            todoList.push(task)
+            console.log(task, todoList)
+            Task
+            }}>
             <Text style ={{color : "white", fontSize : 30, fontWeight : "bold"}}>ADD TODO</Text>
           </TouchableOpacity>
         </View>
@@ -66,7 +88,7 @@ const styles = StyleSheet.create({
     backgroundColor : "#546E7A",
     width : Dimensions.get("window").width /2,
     borderRadius : 15,
-    padding : 10
+    padding : 15
   },
   buttonContainer : {
     backgroundColor : "#B0BEC5",
@@ -75,7 +97,7 @@ const styles = StyleSheet.create({
     alignItems : "center",
     padding : 25,
     marginBottom : 20,
-    height : Dimensions.get("window").height * 0.22
+    height : Dimensions.get("window").height * 0.23
   },
   input : {
     borderWidth : 2,
